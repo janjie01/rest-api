d25722bd8d62a3028709cace518ef8d69634cc07
Janjie Aljas <janjie.aljas1@gmail.com>
2023-11-06 15:03:01 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import Express, { Router, json } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const {API_URL} = process.env;
+const app = Express();
+const router = Router();
+
+app.use(json()).use(
+  router.get("/", function (req, res) {
+    return res.send("Hello from rest-api");
+  }),
+  router.post("/post-request/:id",(req, res) =>{
+    const body = req.body;
+    const params = req.params;
+    const queryParams = req.query;
+
+    res.status(200).json({Body: body,Param: params,"Query params": queryParams});
+  })
+);
+
+app.listen(3000, function () {
+  console.log(API_URL);
+  console.log("Server is running at port 3000");
+});
